2e40b85f2e8a0cf8f47e162f9be27763faf10787
Costa-Silva <julianacostasilvati@gmail.com>
2022-09-19 17:45:28 -0300

    Correção de aula 6

--- 2022/aula6/resultados.php
+++ 2022/aula6/resultados.php
@@ -9,7 +9,7 @@
 <body>
     <h1>Resultados</h1>
     <div>
-        Exercício 1
+    <h2>Exercício 1 - output</h2>
         <?php
         $valor1 = $_POST["valor1"];
         $valor2 = $_POST["valor2"];
@@ -26,6 +26,7 @@
         
     </div>
     <div>
+    <h2>Exercício 2 output</h2>
         <?php
         $texto = "juliana";
         $subtexto = "if";
@@ -34,6 +35,50 @@
         }else{
             echo "$subtexto NÃO esta em $texto";
         }
+        ?>
+    </div>
+    <div>
+        <h2>Exercício 3 - output</h2>
+        <?php
+            for($x=1; $x<=10; $x++){
+                if($x< 10){
+                    echo "$x-";
+                }
+                else{
+                    echo "$x"."<br>";
+                }
+            }
+        ?>
+    </div>
+    <div>
+        <h2>Exercício 4</h2>
+        <?php
+        for($x=1;$x<=5;$x++){
+            for ($y=1;$y<=$x;$y++){
+	            echo "*";
+	            if($y< $x){
+		            echo " ";
+		        }
+            }
+            echo "<br>";
+        }
+        ?>
+    </div>
+    <div>
+        <h2>Exercício 5 - output</h2>
+        <?php
+            for ($row=0; $row<7; $row++){
+                for ($column=0; $column<=7; $column++){
+                    if ($column == 1 or (($row == 0 or $row == 6) and 
+                    ($column > 1 and $column < 6)) or ($row == 3 and $column > 1 and 
+                    $column < 5)){
+                        echo "*";    
+                    }else{
+                        echo " ";     
+                    }
+	            }        
+                echo "<br>";
+            }
         ?>
     </div>
 </body>
